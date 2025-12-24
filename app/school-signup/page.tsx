fa1facc20d220416de8d27defab1f10771df80ba
reelmza <jessemoses71@gmail.com>
2025-12-24 11:58:12 +0100

    Added school signup page.

---
+++ app/school-signup/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Button from "@/components/button";
+import Input from "@/components/input";
+import SideBox from "@/components/sections/side-box";
+import Spacer from "@/components/spacer";
+import { Key, MoveRight, UserRound } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  // Signup Logic
+  const doLogin = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    router.push("/dashboard");
+    return;
+  };
+
+  return (
+    <div className="grid h-full w-full grid-cols-12 font-sans">
+      {/* Side Box */}
+      <SideBox />
+
+      {/* Form Box */}
+      <div className="col-span-6 flex flex-col justify-center items-center">
+        <div className="w-5/10 rounded-lg">
+          {/* School Name */}
+          <div className="text-sm font-sans font-semibold bg-accent-light text-accent-dim w-fit  rounded-full px-3 mb-5 py-1 leading-none">
+            Sauki Sytems
+          </div>
+
+          {/* Form Heading */}
+          <div className="text-2xl font-bold mb-5 text-accent-dim">
+            Login to your Exam.
+          </div>
+
+          <form onSubmit={doLogin}>
+            <Input
+              placeholder="Enter your username"
+              icon={<UserRound size={16} />}
+            />
+            <Spacer size="sm" />
+            <Input placeholder="Enter your password" icon={<Key size={16} />} />
+            <Spacer size="md" />
+
+            <div className="flex items-center text-sm text-accent-dim">
+              Forgot password?
+              <Link
+                href="/reset-password"
+                className="ml-1 inline-block text-accent"
+              >
+                Reset Here.
+              </Link>
+            </div>
+            <Spacer size="md" />
+            <Button
+              title="Proceed to Exam"
+              icon={<MoveRight size={20} strokeWidth={2} />}
+              variant="fill"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
