a716e3c873d9b33ee805b2b308dd3df09945e042
reelmza <jessemoses71@gmail.com>
2026-01-18 01:52:48 +0100

    /assessment/[id] UI

--- app/assessment/[id]/page.tsx
+++ app/assessment/[id]/page.tsx
@@ -1,5 +1,14 @@
 "use client";
+import Button from "@/components/button";
+import Input from "@/components/input";
 import Spacer from "@/components/spacer";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { attachHeaders, localAxios } from "@/lib/axios";
 import { SessionProvider, useSession } from "next-auth/react";
 import { use, useEffect, useState } from "react";
@@ -99,6 +108,114 @@ const Page = ({ id }: { id: string }) => {
                 </div>
               );
             })}
+          </div>
+          <Spacer size="lg" />
+
+          {/* Controls */}
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-6 shadow border rounded-md p-5">
+              {/* Test Status */}
+              <div className="text-sm text-theme-gray">Test Status</div>
+              <Spacer size="sm" />
+
+              <form className="flex items-center gap-4">
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Change Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="published">Published</SelectItem>
+                    <SelectItem value="draft">Draft</SelectItem>
+                  </SelectContent>
+                </Select>
+              </form>
+              <Spacer size="md" />
+
+              {/* Test Duration */}
+              <div className="text-sm text-theme-gray">Test Duration</div>
+              <Spacer size="sm" />
+
+              <form className="flex items-center gap-4">
+                <Input
+                  name="duration"
+                  type="number"
+                  placeholder="Enter duration in minutes"
+                />
+
+                <div className="w-38 shrink-0">
+                  <Button
+                    type="button"
+                    title="Save"
+                    loading={false}
+                    variant="outline"
+                  />
+                </div>
+              </form>
+              <Spacer size="md" />
+
+              {/* Start/End Test */}
+              <div className="flex items-center gap-2">
+                <div className="w-32">
+                  <Button
+                    title="Start Test"
+                    type="button"
+                    variant="fill"
+                    loading={false}
+                  />
+                </div>
+
+                <div className="w-32">
+                  <Button
+                    title="End Test"
+                    type="button"
+                    variant="fillError"
+                    loading={false}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-6 shadow border rounded-md p-5">
+              {/* Assign to group */}
+              <div className="text-sm text-theme-gray">Assign to group</div>
+              <Spacer size="sm" />
+
+              <form className="flex items-center gap-4">
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select Group" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="physc">Physics</SelectItem>
+                    <SelectItem value="architecture">Archi</SelectItem>
+                  </SelectContent>
+                </Select>
+              </form>
+              <Spacer size="md" />
+
+              {/* Assign to students */}
+              {/* Test Duration */}
+              <div className="text-sm text-theme-gray">Asign to student</div>
+              <Spacer size="sm" />
+
+              <form className="flex items-center gap-4">
+                <Input
+                  name="regNumber"
+                  type="text"
+                  placeholder="Enter registration number"
+                />
+
+                <div className="w-38 shrink-0">
+                  <Button
+                    type="button"
+                    title="Asign"
+                    loading={false}
+                    variant="outline"
+                  />
+                </div>
+              </form>
+              <Spacer size="md" />
+            </div>
           </div>
         </div>
       )}
