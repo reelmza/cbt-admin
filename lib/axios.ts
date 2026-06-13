97f0af121deb588a4cd9a2208c36fa6c7eac1ce6
reelmza <jessemoses71@gmail.com>
2026-06-14 00:49:25 +0100

    Deployment fix

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,54 +1,34 @@
 import axios, { AxiosInstance } from "axios";
-import { getApiUrl } from "./getApiURL";
 
 export let localAxios: AxiosInstance;
 
-// export const getAxios = async (): Promise<void> => {
-//   const origin = typeof window !== "undefined" ? window.location.origin : "";
-//   if (origin) {
-//     console.log(origin);
-//     const res = await fetch(`${origin}/api/config`);
-//     const data = await res.json();
-
-//     localAxios = axios.create({
-//       baseURL: data.baseUrl,
-//       timeout: 120_000,
-//     });
-
-//     localAxios.interceptors.response.use(
-//       (response) => response,
-//       (error) => {
-//         if (error?.response?.status === 401) {
-//           window.dispatchEvent(new CustomEvent("session-expired"));
-//         }
-//         return Promise.reject(error);
-//       },
-//     );
-//   } else {
-//     console.log("Unable to get Origin URL");
-//   }
-// };
-
 export const getAxios = async (): Promise<void> => {
-  // const baseURL =
-  //   typeof window === "undefined"
-  //     ? (process.env.SERVER_BASEURL ?? "http://server:4000/api/v1")
-  //     : `http://${window.location.hostname}:4000/api/v1`;
-  console.log(getApiUrl());
-  localAxios = axios.create({
-    baseURL: getApiUrl(),
-    timeout: 120_000,
-  });
+  const origin = typeof window !== "undefined" ? window.location.origin : "";
+  if (origin) {
+    console.log(origin);
+    const res = await fetch(`${origin}/api/config`);
+    const data = await res.json();
 
-  localAxios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error?.response?.status === 401) {
-        window.dispatchEvent(new CustomEvent("session-expired"));
-      }
-      return Promise.reject(error);
-    },
-  );
+    localAxios = axios.create({
+      baseURL: data.baseUrl,
+      timeout: 120_000,
+    });
+
+    localAxios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error?.response?.status === 401) {
+          window.dispatchEvent(new CustomEvent("session-expired"));
+        }
+        return Promise.reject(error);
+      },
+    );
+  } else {
+    console.log(
+      origin,
+      "No origin found, skipping axios configuration for this instance",
+    );
+  }
 };
 
 getAxios();
